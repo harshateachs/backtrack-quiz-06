8ed35efad782a4fae101a36b09e5969f01dc42c7
Harsha Abeyvickrama <harshaabeyvickrama@gmail.com>
2024-04-10 20:25:24 +0530

    Implement the answer

--- app.py
+++ app.py
@@ -2,23 +2,20 @@ from typing import List
 
 
 def list_operations(list1, list2, operations):
-    """
-    Perform various operations on two lists based on the provided operations string.
-
-    Args:
-    - list1 (List[int]): The first list of integers.
-    - list2 (List[int]): The second list of integers.
-    - operations (str): A string containing a sequence of characters representing operations to be performed.
-                        Each character represents an operation:
-                        'M' - Merge the lists.
-                        'R' - Remove duplicates.
-                        'S' - Sort the list.
-                        'V' - Reverse the list.
-
-    Returns:
-    - List[int]: The result list after performing the specified operations.
-    """
-    pass
+    merged_list = list1
+    merged_list.extend(list2)
+    for op in operations:
+        if op == "M":
+            pass
+        elif op == "R":
+            merged_list = list(dict.fromkeys(merged_list))
+        elif op == "S":
+            merged_list.sort()
+        elif op == "V":
+            merged_list.sort(reverse=True)
+        else:
+            raise ValueError("Unknown Operator")
+    return merged_list
 
 
 # Sample test cases
@@ -27,7 +24,7 @@ if __name__ == "__main__":
     assert list_operations([1, 3, 2], [4, 3, 5], 'MRS') == [1, 2, 3, 4, 5]
     assert list_operations([7, 5, 3], [4, 2, 1], 'RMV') == [7, 5, 4, 3, 2, 1]
     assert list_operations([4, 1, 6], [2, 5, 3], 'MS') == [1, 2, 3, 4, 5, 6]
-    assert list_operations([10, 20, 30], [30, 40, 50], 'MRV') == [50, 40, 10, 20, 30]
+    assert list_operations([10, 20, 30], [30, 40, 50], 'MRV') == [50, 40, 30,20,10]
     assert list_operations([3, 2, 1], [4, 5, 4], 'RSM') == [1, 2, 3, 4, 5]
 
     print("All test cases passed!")
