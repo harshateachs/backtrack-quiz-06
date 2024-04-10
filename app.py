1d71a84b0b96f0da0b240e56cfb8e66e3bf63bfa
Harsha Abeyvickrama <harshaabeyvickrama@gmail.com>
2024-04-10 20:26:24 +0530

    Update the assertions

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     assert list_operations([1, 3, 2], [4, 3, 5], 'MRS') == [1, 2, 3, 4, 5]
     assert list_operations([7, 5, 3], [4, 2, 1], 'RMV') == [7, 5, 4, 3, 2, 1]
     assert list_operations([4, 1, 6], [2, 5, 3], 'MS') == [1, 2, 3, 4, 5, 6]
-    assert list_operations([10, 20, 30], [30, 40, 50], 'MRV') == [50, 40, 10, 20, 30]
+    assert list_operations([10, 20, 30], [30, 40, 50], 'MRV') == [50, 40, 30, 20, 10]
     assert list_operations([3, 2, 1], [4, 5, 4], 'RSM') == [1, 2, 3, 4, 5]
 
     print("All test cases passed!")
